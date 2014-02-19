f5e9e98c57b019f665c49ce9ff771e9671f9b024
houn <good.sergio@gmail.com>
2014-02-19 23:12:14 +0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rails3BootstrapDeviseCancan::Application.config.secret_token = 'd7b7f7f995c2c24e4539ce2de043db7780804fc1148d5c4677b90563f566838158f56fcff34f69cb900f668e948b038c032b4e35165c5885d53c3362b7e89728'
